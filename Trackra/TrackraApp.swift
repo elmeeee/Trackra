cea31869dd59fee50580492560a037f6ae7b2c4c
elmeeee <37185272+elmeeee@users.noreply.github.com>
2026-02-01 13:23:51 +0700

    Initial Commit

---
+++ Trackra/TrackraApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackraApp.swift
+//  Trackra
+//
+//  Created by Elmee on 01/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct TrackraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
